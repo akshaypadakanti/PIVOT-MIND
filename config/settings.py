480cdc43cb98bf97dd937ea15dc1a940e89725d8
akshaypadakanti <akshaypadakanti06@gmail.com>
2026-09-16 10:37:41 +0530

    Fix_Static_Files_Storage

--- config/settings.py
+++ config/settings.py
@@ -119,9 +119,10 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
